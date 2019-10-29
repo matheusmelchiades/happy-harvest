7deffc872f4d8a42020ebed24e997881a94f49ed
Matheus Melchiades <matheusmacielmelchiades@hotmail.com>
2019-10-29 17:23:15 -0300

    add a autocomplete

---
+++ src/components/Forms/components/AutoComplete/index.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+
+import { Autocomplete } from '@material-ui/lab';
+import { Input } from '../Input/styles';
+
+export default function({ onChange, input, getOptionLabel, options = [] }) {
+    const [open, setOpen] = useState(false);
+
+    console.log(options);
+
+    return (
+        <Autocomplete
+            open={open}
+            onOpen={() => setOpen(true)}
+            onClose={() => setOpen(false)}
+            getOptionLabel={getOptionLabel}
+            options={options}
+            onChange={onChange}
+            renderInput={params => <Input {...params} {...input} />}
+        />
+    );
+}
